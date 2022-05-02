57c927ac06f96dbc788ceb3b75d4afbdd00c1df5
AkashdeepArjun <wadhwaarjun007@gmail.com>
2022-05-02 03:26:06 -0700

    colors that implement color interface

---
+++ src/home/GreenColor.java
@@ -0,0 +1,9 @@
+package home;
+
+public class GreenColor implements Color {
+    @Override
+    public void applyColor() {
+      MyUtils.log("GREEN COLOR  ","color applied");
+        
+    }
+}
---
+++ src/home/RedColor.java
@@ -0,0 +1,9 @@
+package home;
+
+public class RedColor implements Color {
+    @Override
+    public void applyColor() {
+      MyUtils.log("RED COLOR  ","color applied");
+        
+    }
+}
